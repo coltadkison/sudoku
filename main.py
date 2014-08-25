90e15efa93d49c91071a3ced1e7d1c841b4e1dbb
coltadkison <coltadkison@users.noreply.github.com>
2014-08-24 23:40:32 -0500

    Included test puzzle from Websudoku.

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ __author__ = 'Colt'
 from sudoku_board import SudokuBoard
 from sudoku_solver import SudokuSolver
 
-#Websudoku Evil Puzzle 9,631,808,478
+# Websudoku Evil Puzzle 9,631,808,478
 TEST_BOARD = [
-    0,0,0,6,2,0,1,0,0,
-    0,0,9,0,0,3,0,7,0,
-    4,0,0,0,7,0,0,0,0,
-    0,2,0,0,0,0,9,6,0,
-    0,8,1,0,0,0,7,5,0,
-    0,4,6,0,0,0,0,3,0,
-    0,0,0,0,3,0,0,0,9,
-    0,6,0,5,0,0,4,0,0,
-    0,0,8,0,1,6,0,0,0
+    0, 0, 0, 6, 2, 0, 1, 0, 0,
+    0, 0, 9, 0, 0, 3, 0, 7, 0,
+    4, 0, 0, 0, 7, 0, 0, 0, 0,
+    0, 2, 0, 0, 0, 0, 9, 6, 0,
+    0, 8, 1, 0, 0, 0, 7, 5, 0,
+    0, 4, 6, 0, 0, 0, 0, 3, 0,
+    0, 0, 0, 0, 3, 0, 0, 0, 9,
+    0, 6, 0, 5, 0, 0, 4, 0, 0,
+    0, 0, 8, 0, 1, 6, 0, 0, 0
 ]
 if __name__ == "__main__":
     square_size = 3
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     for i in range(my_board.num_rows):
         for j in range(my_board.num_cols):
             my_board.set_square(i, j, TEST_BOARD[(my_board.num_rows * i) + j])
-    # my_board.print_board()
+    my_board.print_board()
     my_solver = SudokuSolver(my_board)
     my_solver.recursive_solver(0, 0)
     my_board.print_board()

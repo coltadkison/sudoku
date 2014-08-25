2b0de62f253b205c852de4fdf857525ecdc5be53
coltadkison <coltadkison@users.noreply.github.com>
2014-08-24 23:39:17 -0500

    Included test puzzle from Websudoku.

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 __author__ = 'Colt'
 
 from sudoku_board import SudokuBoard
+from sudoku_solver import SudokuSolver
 
+#Websudoku Evil Puzzle 9,631,808,478
+TEST_BOARD = [
+    0,0,0,6,2,0,1,0,0,
+    0,0,9,0,0,3,0,7,0,
+    4,0,0,0,7,0,0,0,0,
+    0,2,0,0,0,0,9,6,0,
+    0,8,1,0,0,0,7,5,0,
+    0,4,6,0,0,0,0,3,0,
+    0,0,0,0,3,0,0,0,9,
+    0,6,0,5,0,0,4,0,0,
+    0,0,8,0,1,6,0,0,0
+]
 if __name__ == "__main__":
     square_size = 3
-    rows = 9
-    cols = 9
-    my_board = SudokuBoard(3, rows, cols, '-')
-    row,col = my_board.find_corner_sudoku_square(4,5)
-    my_board.print_sudoku_square(row,col)
+    num_rows = 9
+    num_cols = 9
+    default_num = 0
+    my_board = SudokuBoard(3, num_rows, num_cols, 0)
+    for i in range(my_board.num_rows):
+        for j in range(my_board.num_cols):
+            my_board.set_square(i, j, TEST_BOARD[(my_board.num_rows * i) + j])
+    # my_board.print_board()
+    my_solver = SudokuSolver(my_board)
+    my_solver.recursive_solver(0, 0)
+    my_board.print_board()

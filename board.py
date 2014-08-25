3a80d31b72dbdb393e0d3681fe224ed6d35e5ee1
coltadkison <coltadkison@users.noreply.github.com>
2014-08-24 21:38:51 -0500

    Temporarily set the default value to the coordinates of the board square for testing

--- board.py
+++ board.py
@@ -13,7 +13,7 @@ class Board():
         def reset_board(self):
             for i in range(self.num_rows):
                 for j in range(self.num_cols):
-                    self.set_square(i, j, self.default_value)
+                    self.set_square(i, j, ','.join((str(i),str(j))))
 
         #Print entire board
         def print_board(self):

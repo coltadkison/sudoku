1f8545948c4d96d0ce2b75536010850510ea7861
coltadkison <coltadkison@users.noreply.github.com>
2014-08-24 23:33:37 -0500

    Completed recursive brute force solver.

---
+++ sudoku_solver.py
@@ -0,0 +1,58 @@
+__author__ = 'colt'
+
+
+class SudokuSolver():
+    def __init__(self, sudoku_board):
+        self.sudoku_board = sudoku_board
+
+    def __check_row(self, row):
+        row_squares = [each for each in self.sudoku_board.get_row(row) if each != 0]
+        return len(row_squares) == len(set(row_squares))
+
+    def __check_col(self, col):
+        col_squares = [each for each in self.sudoku_board.get_col(col) if each != 0]
+        return len(col_squares) == len(set(col_squares))
+
+    def __check_square(self, row, col):
+        square_squares = [each for each in self.sudoku_board.get_sudoku_square(row, col) if each != 0]
+        return len(square_squares) == len(set(square_squares))
+
+    def check_board(self):
+        for i in range(self.sudoku_board.num_rows):
+            if not self.__check_row(i):
+                return False
+        for i in range(self.sudoku_board.num_cols):
+            if not self.__check_col(i):
+                return False
+        # noinspection PyArgumentList
+        for i in range(0, self.sudoku_board.num_rows, self.sudoku_board.square_size):
+            for j in range(0, self.sudoku_board.num_cols, self.sudoku_board.square_size):
+                if not self.__check_square(i, j):
+                    return False
+        return True
+
+    def recursive_solver(self, row, col):
+        if row == self.sudoku_board.num_rows:
+            return True
+        elif col == self.sudoku_board.num_cols:
+            return self.recursive_solver(row+1, 0)
+        if self.sudoku_board.get_square(row, col) != 0:
+            return self.recursive_solver(row, col+1)
+        else:
+            all_values = set([each for each in range(1, 10)])
+            possible_values = all_values.difference(self.sudoku_board.get_row(row),
+                                                    self.sudoku_board.get_col(col),
+                                                    self.sudoku_board.get_sudoku_square(row, col))
+            if not possible_values:
+                return False
+            for each in possible_values:
+                self.sudoku_board.set_square(row, col, each)
+                if self.check_board():
+                    if self.recursive_solver(row, col+1):
+                        return True
+                else:
+                    continue
+            self.sudoku_board.set_square(row, col, 0)
+            return False
+
+

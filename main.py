35f1df31ec45f6167caf4da4abb82b4ec50c74a9
coltadkison <coltadkison@users.noreply.github.com>
2014-08-24 21:38:29 -0500

    Added some test values to show the proper Sudoku square is being printed.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ __author__ = 'Colt'
 from sudoku_board import SudokuBoard
 
 if __name__ == "__main__":
-        rows = 9
-        cols = 9
-        my_board = SudokuBoard(rows, cols, '-')
+    square_size = 3
+    rows = 9
+    cols = 9
+    my_board = SudokuBoard(3, rows, cols, '-')
+    row,col = my_board.find_corner_sudoku_square(4,5)
+    my_board.print_sudoku_square(row,col)

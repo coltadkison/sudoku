89304c1b8c928cb4991fe1700c2bfe89d78b4f60
coltadkison <coltadkison@users.noreply.github.com>
2014-08-24 21:06:37 -0500

    Added comments to all functions.

--- board.py
+++ board.py
@@ -9,20 +9,24 @@ class Board():
             self.board = [None] * rows * cols
             self.reset_board()
 
+        #Reset all squares in board to default_value
         def reset_board(self):
             for i in range(self.num_rows):
                 for j in range(self.num_cols):
                     self.set_square(i, j, i)
 
+        #Print entire board
         def print_board(self):
             for i in range(self.num_rows):
                 for j in range(self.num_cols):
                     print(self.board[(self.num_rows * i) + j], end=' ')
                 print()
 
+        #Return the value of a single square on the board
         def get_square(self, row, col):
             return self.board[(self.num_rows * row) + col]
 
+        #Set the value of a single square on the board
         def set_square(self, row, col, value):
             self.board[(self.num_rows * row) + col] = value
 

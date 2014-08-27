0e8b33b6b43ac52275606e4a8582e2053e5766dc
traptInReality <traptInReality@users.noreply.github.com>
2014-08-26 21:48:27 -0500

    Added a smaller test board for a quicker solution.

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from sudoku_board import SudokuBoard
 from sudoku_solver import SudokuSolver
 
 # Websudoku Evil Puzzle 9,631,808,478
+# TEST_BOARD = [
+#    0, 0, 0, 6, 2, 0, 1, 0, 0,
+#    0, 0, 9, 0, 0, 3, 0, 7, 0,
+#    4, 0, 0, 0, 7, 0, 0, 0, 0,
+#    0, 2, 0, 0, 0, 0, 9, 6, 0,
+#    0, 8, 1, 0, 0, 0, 7, 5, 0,
+#    0, 4, 6, 0, 0, 0, 0, 3, 0,
+#    0, 0, 0, 0, 3, 0, 0, 0, 9,
+#    0, 6, 0, 5, 0, 0, 4, 0, 0,
+#    0, 0, 8, 0, 1, 6, 0, 0, 0
+#]
+
 TEST_BOARD = [
-    0, 0, 0, 6, 2, 0, 1, 0, 0,
-    0, 0, 9, 0, 0, 3, 0, 7, 0,
-    4, 0, 0, 0, 7, 0, 0, 0, 0,
-    0, 2, 0, 0, 0, 0, 9, 6, 0,
-    0, 8, 1, 0, 0, 0, 7, 5, 0,
-    0, 4, 6, 0, 0, 0, 0, 3, 0,
-    0, 0, 0, 0, 3, 0, 0, 0, 9,
-    0, 6, 0, 5, 0, 0, 4, 0, 0,
-    0, 0, 8, 0, 1, 6, 0, 0, 0
+    7, 0, 0, 1, 5, 0, 0, 8, 4,
+    0, 0, 0, 0, 7, 4, 1, 0, 0,
+    0, 5, 0, 8, 0, 9, 6, 2, 0,
+    2, 1, 6, 0, 0, 0, 0, 0, 0,
+    0, 0, 5, 0, 0, 0, 2, 0, 0,
+    0, 0, 0, 0, 0, 0, 9, 5, 3,
+    0, 8, 2, 4, 0, 1, 0, 3, 0,
+    0, 0, 3, 9, 8, 0, 0, 0, 0,
+    9, 4, 0, 0, 2, 5, 0, 0, 1,
 ]
 if __name__ == "__main__":
     square_size = 3
@@ -24,7 +36,7 @@ if __name__ == "__main__":
     for i in range(my_board.num_rows):
         for j in range(my_board.num_cols):
             my_board.set_square(i, j, TEST_BOARD[(my_board.num_rows * i) + j])
-    my_board.print_board()
+
     my_solver = SudokuSolver(my_board)
     my_solver.recursive_solver(0, 0)
     my_board.print_board()

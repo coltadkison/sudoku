011aafe6dfd1939163f5d709bd296fa8fdbb66d6
traptInReality <traptInReality@users.noreply.github.com>
2014-09-03 22:40:45 -0500

    Removing BoardSquare because it was a terrible idea.

--- board.py
+++ board.py
@@ -12,25 +12,23 @@ class Board():
 
         #Reset all squares in board to default_value
         def reset_board(self):
-            self.board = [BoardSquare(row, col, self.default_value)
-                          for row in range(self.num_rows)
-                          for col in range(self.num_cols)]
+            self.board = [self.default_value * self.num_rows * self.num_cols]
 
         def reset_square(self, row, col):
-            self.set_square(row, col, BoardSquare(row, col, self.default_value))
+            self.set_square(row, col, self.default_value)
 
         #Print entire board
         def print_board(self):
             for row in range(self.num_rows):
-                print(' '.join([str(square.get_value()) for square in self.get_row(row)]))
+                print(' '.join(map(str,self.get_row(row))))
 
         #Return the value of a single square on the board
         def get_square(self, row, col):
             return self.board[(self.num_rows * row) + col]
 
         #Set the value of a single square on the board
-        def set_square(self, row, col, square):
-            self.board[(self.num_rows * row) + col] = square
+        def set_square(self, row, col, value):
+            self.board[(self.num_rows * row) + col] = value
 
         def get_row(self, row):
             return [self.get_square(row, col) for col in range(self.num_cols)]
